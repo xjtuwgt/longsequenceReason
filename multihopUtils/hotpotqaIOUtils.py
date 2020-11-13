d23fd4512e8b26ec15ef6a58199752f6773e9e3d
guangtao.wang <guangtao.wang@jd.com>
2020-11-12 16:10:40 -0800

    update

--- multihopUtils/hotpotqaIOUtils.py
+++ multihopUtils/hotpotqaIOUtils.py
@@ -1,9 +1,9 @@
+# import os
+# import sys
+# PACKAGE_PARENT = '..'
+# SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+# sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 import os
-import sys
-PACKAGE_PARENT = '..'
-SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
-sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
-
 import pandas as pd
 from pandas import DataFrame
 import torch
