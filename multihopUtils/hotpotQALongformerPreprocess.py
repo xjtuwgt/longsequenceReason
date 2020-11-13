3bfa26f581b54d81648658a71d44b43b5e7e412a
guangtao.wang <guangtao.wang@jd.com>
2020-11-12 17:48:17 -0800

    update

--- multihopUtils/hotpotQALongformerPreprocess.py
+++ multihopUtils/hotpotQALongformerPreprocess.py
@@ -98,7 +98,7 @@ def Hotpot_Train_Data_Preprocess(data: DataFrame, tokenizer: LongformerQATensori
     #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     start_time = time()
     data[['norm_query', 'norm_answer', 'p_ctx', 'n_ctx', 'supp_facts_filtered', 'p_doc_type', 'p_doc_num',
-          'n_doc_num', 'yes_no', 'no_found']] = data.swifter.progress_bar(True)(lambda row: pd.Series(pos_neg_context_split(row)), axis=1)
+          'n_doc_num', 'yes_no', 'no_found']] = data.swifter.progress_bar(True).apply(lambda row: pd.Series(pos_neg_context_split(row)), axis=1)
     not_found_num = data[data['no_found']].shape[0]
     print('Splitting positive samples from negative samples takes {:.4f} seconds, answer not found = {}'.format(time() - start_time, not_found_num))
     # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -350,7 +350,7 @@ def Hotpot_Test_Data_PreProcess(data: DataFrame, tokenizer: LongformerQATensoriz
         return norm_question, hotpot_ctxs
     # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     start_time = time()
-    data[['norm_query', 'norm_ctx']] = data.swifter.progress_bar(True)(lambda row: pd.Series(norm_context(row)), axis=1)
+    data[['norm_query', 'norm_ctx']] = data.swifter.progress_bar(True).apply(lambda row: pd.Series(norm_context(row)), axis=1)
     print('Normalizing samples takes {:.4f} seconds'.format(time() - start_time))
     # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     def row_encoder(row):
@@ -381,7 +381,7 @@ def Hotpot_Test_Data_PreProcess(data: DataFrame, tokenizer: LongformerQATensoriz
     # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     start_time = time()
     data[['ques_encode', 'ques_len', 'ctx_encode', 'ctx_lens', 'ctx_max_len']] = \
-        data.apply(lambda row: pd.Series(row_encoder(row)), axis=1)
+        data.swifter.progress_bar(True).apply(lambda row: pd.Series(row_encoder(row)), axis=1)
     print('Tokenizing takes {:.4f} seconds'.format(time() - start_time))
     print('Number of data = {}'.format(data.shape))
     return data
