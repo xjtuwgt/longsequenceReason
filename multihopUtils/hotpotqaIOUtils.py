eda6a65ddc1055a77cdbe1ca5ca3dfc9cdbcbe24
guangtao.wang <guangtao.wang@jd.com>
2020-11-12 16:27:21 -0800

    update

--- multihopUtils/hotpotqaIOUtils.py
+++ multihopUtils/hotpotqaIOUtils.py
@@ -1,8 +1,8 @@
-# import os
-# import sys
-# PACKAGE_PARENT = '..'
-# SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
-# sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+import os
+import sys
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 import os
 import pandas as pd
 from pandas import DataFrame
