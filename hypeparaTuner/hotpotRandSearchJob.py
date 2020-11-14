7e4b78d6d04bb208096a78050fe8e92d2ecba7be
guangtao.wang <guangtao.wang@jd.com>
2020-11-14 15:27:56 -0800

    update

--- hypeparaTuner/hotpotRandSearchJob.py
+++ hypeparaTuner/hotpotRandSearchJob.py
@@ -55,7 +55,7 @@ def generate_random_search_bash(task_num):
     search_space = HypeParameterSpace()
     random_search_job =RandomSearchJob(search_space=search_space)
     for i in range(task_num):
-        task_id, parameter_id = random_search_job.single_task_trial(i+100)
+        task_id, parameter_id = random_search_job.single_task_trial(i+500)
         with open(bash_save_path + 'run_' + task_id +'.sh', 'w') as rsh_i:
             command_i = 'bash qarun.sh ' + parameter_id
             rsh_i.write(command_i)
